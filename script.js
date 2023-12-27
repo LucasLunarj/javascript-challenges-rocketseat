a075656b5f9042fd974523cdc635322d18ca71ab
LucasLunarj <87677530+LucasLunarj@users.noreply.github.com>
2023-12-27 00:12:10 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,94 @@
+// 1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+
+// alert("Hello World!")
+
+// 2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+
+// const num1 = 20;
+// const num2 = 31;
+
+// alert(`O resultado da soma é ${num1 + num2} `)
+
+// 3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+
+/* const data = 10
+
+if (typeof data === 'number') {
+    alert("É um número")
+} else {
+    alert("Não é um número")
+}
+*/
+
+// 4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+/*
+const data = "casa"
+
+if (typeof data === 'number') {
+    alert("É uma string")
+} else {
+    alert("Não é uma string")
+}
+*/
+
+// 5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+
+/*
+const value = true
+
+if (typeof value === 'boolean') {
+    alert("É um booleano")
+} else {
+    alert("Não é um booleano")
+}
+*/
+
+// 6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+
+/*
+const num1 = prompt('Digite um número:')
+const num2 = prompt('Digite outro número:')
+
+alert(`O resultado da subtração é: ${Number(num1) - Number(num2)}`)
+*/
+
+// 7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+
+/*
+const num1 = prompt('Digite um número:')
+const num2 = prompt('Digite outro número:')
+
+alert(`O resultado da subtração é: ${Number(num1) * Number(num2)}`)
+*/
+
+// 8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+/*
+const num1 = prompt('Digite um número:')
+const num2 = prompt('Digite outro número:')
+
+alert(`O resultado da subtração é: ${Number(num1) / Number(num2)}`)
+*/
+
+// 9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+/*
+const num = prompt('digite um número')
+result = num % 2
+
+if (result === 0) {
+    alert("É um número par")
+} else {
+    alert("Não é um número par")
+}
+*/
+
+// 10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar.Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+
+const num = prompt('digite um número')
+result = num % 2
+
+if (result !== 0) {
+    alert("É um número ímpar")
+} else {
+    alert("Não é um número ímpar")
+}
+
